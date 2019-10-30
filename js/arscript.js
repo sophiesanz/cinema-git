a6d8320aba7cd1c3423be7d7c78ab8566ed1e744
Sofia Sanchez <sofiasanches@sofiasanches-macbookpro.roam.corp.google.com>
2019-10-29 22:18:32 -0500

    new folder version

--- js/arscript.js
+++ js/arscript.js
@@ -1,6 +1,7 @@
 var arteatros = {"Bogotá":["Unicentro","Teatro Norte","Teatro Sur"],"Cali":["Chipichape","Unico","Viva"],"Medellin":["Aventura Plaza","Bosque Plaza","Oviedo"]};
 var arpelis = {"Unicentro":["Malefica","Geminis","Abominable","Joker", "Agente 00-Gato"],"Teatro Norte":["Malefica","Abominable","Joker"], "Teatro Sur":["Malefica","Joker"], "Chipichape":["Geminis","Abominable","Joker"],"Unico":["Malefica","Joker"], "Viva":["Malefica","Agente 00-Gato"], "Aventura Plaza":["Malefica","Abominable","Joker"],"Bosque Plaza":["Agente 00-Gato","Abominable","Joker"], "Oviedo":["Geminis","Malefica"]};
 var arhora={"Malefica":["2:00 pm","4:00 pm","6:00 pm","8:00 pm"],"Geminis":["2:30 pm","4:40 pm","6:55 pm", "9:10 pm"], "Abominable":["1:00 pm","3:00 pm","5:00 pm", "7:00 pm"], "Joker":["2:30 pm","5:40 pm","7:55 pm", "9:20 pm"],"Agente 00-Gato":["1:30 pm","3:40 pm","5:55 pm", "7:10 pm"]}
+var arenlaces={"Malefica":"malefica.html", "Geminis":"geminis.html", "Abominable":"abominable.html", "Joker":"guason.html","Agente 00-Gato":"gato.html"};
 function arCityChange() {
     var aroptions = document.getElementById('city').options;
     var arindex = aroptions.selectedIndex;
@@ -12,6 +13,8 @@ function arCityChange() {
     blanquear(arSelectTeatro);
     blanquear(document.getElementById('arpeli'));
     blanquear(document.getElementById('arhora'));
+    document.getElementById("arbutton").disabled=true;
+    arSelectTeatro.disabled=false;
 
     for (let i = 0; i < arListaTeatros.length; i++) {
         const arTeatroFor = arListaTeatros[i];
@@ -31,6 +34,8 @@ function arTeatroChange() {
     var arpelisSelect = document.getElementById('arpeli');
     blanquear(arpelisSelect);
     blanquear(document.getElementById('arhora'));
+    document.getElementById("arbutton").disabled=true;
+    arpelisSelect.disabled=false;
 
     for (let i = 0; i < arListPelis.length; i++) {
         const arPelifor = arListPelis[i];
@@ -47,10 +52,16 @@ function arPeliChange() {
 
     var arListHora = arhora[arPeliElegida];
 
+    if (arindex==0){
+        document.getElementById("arbutton").disabled=true;
+    } else{
+        document.getElementById("arbutton").disabled=false;
+    }
+
     console.log(arPeliElegida);
     var arhoraSelect = document.getElementById('arhora');
     blanquear(arhoraSelect);
-
+    arhoraSelect.disabled=false;
     for (let i = 0; i < arListHora.length; i++) {
         const arHorafor = arListHora[i];
         var option = document.createElement("option");
@@ -59,8 +70,26 @@ function arPeliChange() {
     }
 }
 function blanquear(arSelect) {
+    arSelect.disabled=true;
+    document.getElementById("")
     var artamano = arSelect.length;
     for (var i = 1 ; i < artamano; i++) {
             arSelect.remove(1);
     }
+}
+
+function arverpeli() {
+
+    var aroptions = document.getElementById('arpeli').options;
+    var arindex = aroptions.selectedIndex;
+    var arPeliElegida = aroptions[arindex].text;
+    var arenlace=arenlaces[arPeliElegida];
+    window.location.href = arenlace;
+    
+   /*  if (arindex!=0){
+        window.location.href = arenlace;
+        
+    } else{
+        alert("Seleccione una pelicula")
+    } */
 }
